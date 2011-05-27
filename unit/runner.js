3090f9a622d43f2a77c09383fc0764dbf8bb950b
pyrsmk <dev@dreamysource.fr>
2011-05-27 09:20:25 +0200

    Update to 0.1.6

--- unit/runner.js
+++ unit/runner.js
@@ -1,25 +1,18 @@
 sink('ClassIE',function(test,ok,before,after){
     
     var expected=prompt("What IE version should I expect? (please let empty for other browsers)","");
+    if(expected==''){
+        expected=undefined;
+    }
     
     test('Browser version',2,function(){
-        try{
-            ok(IE==expected,"same JS version");
-        }
-        catch(e){
-            if(expected===''){
-                ok(true,"same JS version");
-            }
-            else{
-                ok(false,"same JS version");
-            }
-        }
+        ok(IE==expected,"same JS version");
         var classes=document.documentElement.className;
         if(classes===undefined){
             classes='';
         }
         var found=classes.match(/\bie(\d+(\.5)?)\b/);
-        ok((found?found[1]:'')==expected,"same CSS version");
+        ok((found?found[1]:undefined)==expected,"same CSS version");
     });
     
 });
